ca09d2217479f309af0e401305ceec1bbc78462f
mc22dg <mc22dg@student.lnu.se>
2014-10-02 13:17:25 +0200

    Lagt till variabler, metoder m.m.

---
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L1A
+{
+    public class SecretNumber
+    {
+        //Deklaration av konstant och varaibler.
+        public const int MaxNumberOfGuesses = 7;
+        private int _count;
+        private int _number;
+
+
+        public void Initialize()
+        {
+            Random myRandom = new Random();
+            _number = myRandom.Next(1, 101);
+            _count = 0;
+            
+    
+        }
+
+
+        public bool MakeGuess()
+        {
+
+        }
+
+    }
+}
